b54305757be07e8fda183bc55e8604a130600320
Abid ANIS <anab.tn@gmail.com>
2014-06-11 17:54:30 +0100

    update plugin disconnect.img.js

--- assets/js/jquery.disconnect.img.js
+++ assets/js/jquery.disconnect.img.js
@@ -1,49 +1,60 @@
-(function($) {
-    $.fn.DesconnectImg = function(option) {   
+(function (window, $) {
+    'use strict';
+    $.fn.DesconnectImg = function (option) {
         option = $.extend({
             version : "1.1.1",
             debug : true,
             modal : {},
             filesystem : {},
-            consolelog :{
-                css : 'background: #DDD; color: #F60'
-            },
-            callback : function() {}
+            consolelog : {},
+            callback : function () { return false; }
         }, option || {});
+
+        option.consolelog = $.extend({
+            css : 'background: #DDD; color: #F60'
+        }, option.consolelog);
 
         option.filesystem = $.extend({
             storage_type : 'persistent' // temporary
-        },option.filesystem);
+        }, option.filesystem);
 
-        
+        // var LOCAL_FILE_BASEURL = "filesystem:" + window.location.origin + "/" + option.filesystem.storage_type + "/";
 
-        this.initload = function(){
-            console.log("xxxxxxxxxxx");
-        };
+        this.extend({
+            /**
+             * Log
+             *
+             * @author 2A
+             * @this {DesconnectImg}
+             * @function log
+             * @return {void} the DesconnectImg
+             */
+            log : function (msg) {
+                if (option.debug) {
+                    try { window.console.log('%cREMOT LOG:', option.consolelog.css, msg); } catch (e) { window.alert(msg); }
+                }
+            },
+            /**
+             * Init
+             *
+             * @author 2A
+             * @this {DesconnectImg}
+             * @function init
+             * @return {void} the DesconnectImg
+             */
+            init : function () {
+                this.log('Plugin "DesconnectImg" , methode "init"');
+            }
 
-        
+        });
+
+        this.init();
 
         // traverse all nodes
-        this.each(function() {
-
+        this.each(function () {
             // express a single node as a jQuery object
-            var $this = $(this),
-            LOCAL_FILE_BASEURL = "filesystem:" + window.location.origin + "/"+option.filesystem.storage+"/";
-
+            var $this = $(this);
             $this.fn = {
-                /**
-                 * Log
-                 *
-                 * @author 2A
-                 * @this {DesconnectImg}
-                 * @function log
-                 * @return {void} the DesconnectImg
-                 */
-                 log : function(msg){
-
-                    if(option.debug)
-                        try { console.log('%cREMOT LOG:', option.consolelog.css ,msg); } catch (e) { alert(msg) }
-                },
                 /**
                  * Init
                  *
@@ -52,13 +63,10 @@
                  * @function init
                  * @return {void} the DesconnectImg
                  */
-                 init : function(){
-                    $this.fn.log('Plugin "DesconnectImg" , methode "init"');
-
-                },
-
-
-            } 
+                init : function () {
+                    this.log('Plugin "DesconnectImg" , methode "init"');
+                }
+            };
             // end fn
             /**
              * $this.fn.Init
@@ -68,22 +76,9 @@
              * @function invoke $this.fn.init
              * @return {void} the DesconnectImg
              */
-             $this.fn.init();
-
-         });
-
+            $this.fn.init();
+        });
         // allow jQuery chaining
         return false;
-    }
-
-    
-})(jQuery);
-
-/**
- * index  module
- */
-
-(function () {
-    console.log('index js executed');
-    $('img').DesconnectImg();
-})();
+    };
+})(window, jQuery);
